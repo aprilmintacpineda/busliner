9e9b355229aa834dd98712543197ed34140c3dc2
April Pineda <aprilmintacpineda@gmail.com>
2017-05-21 13:28:48 +0800

    initial setup

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+
+  <title>Laravel</title>
+</head>
+<body>
+  <div id="main"></div>
+  <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const webpack = require('webpack');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+
+var uglifyJsPlugin = process.env.PROD == 1? [
+  new webpack.optimize.UglifyJsPlugin({
+    sourceMap: false,
+    output: {
+      comments: false
+    },
+    compressor: {
+      warnings: false
+    }
+  }),
+
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify('production')
+    }
+  })
+] : [];
+
+var uglifyCssPlugin = process.env.PROD == 1? [ new OptimizeCssAssetsWebpackPlugin() ] : []
+
+module.exports = [
+  {
+    name: 'javascript',
+    entry: __dirname + '/resources/assets/js/entry.js',
+    output: {
+      filename: 'app.js',
+      path: __dirname + '/public/js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query: {
+            presets: ['es2015', 'react'],
+            plugins: ['babel-plugin-transform-object-rest-spread']
+          }
+        }
+      ]
+    },
+    plugins: uglifyJsPlugin
+  },
+
+  {
+    name: 'stylesheets',
+    entry: __dirname + '/resources/assets/sass/entry.sass',
+    output: {
+      filename: 'app.css',
+      path: __dirname + '/public/css'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.sass/,
+          loader: ExtractTextWebpackPlugin.extract({
+            use: 'css-loader!sass-loader',
+            fallback: 'style-loader'
+          })
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextWebpackPlugin('app.css')
+    ].concat(uglifyCssPlugin)
+  }
+];
